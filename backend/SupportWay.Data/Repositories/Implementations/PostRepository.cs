e7071818080677aefa7f580cbe05514237963eb0
dmytro-babiak <dmytro.babiak.pz.2022@lpnu.ua>
2025-08-01 13:04:39 +0300

    Add PostRepository with basic CRUD operation

---
+++ backend/SupportWay.Data/Repositories/Implementations/PostRepository.cs
@@ -0,0 +1,67 @@
+﻿using SupportWay.Data.Context;
+using SupportWay.Data.Models;
+using SupportWay.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupportWay.Data.Repositories.Implementations
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly SupportWayContext _context;
+
+        public PostRepository(SupportWayContext context)
+        {
+            _context = context;
+        }
+        public async Task AddPostAsync(Post post)
+        {
+            await _context.Posts.AddAsync(post);
+        }
+
+        public async Task DeletePostAsync(int postId)
+        {
+           var post = await _context.Posts.FindAsync(postId);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Post?> GetPostByIdAsync(string postId)
+        {
+            return await _context.Posts.FindAsync(postId);
+        }
+
+        public async Task<IEnumerable<Post>> GetPostByUserAsync(string userId, int pageNumber, int pageSize)
+        {
+            return await _context.Posts
+                .Where(p => p.UserId == userId)
+                .OrderByDescending(p => p.CreatedAt)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Post>> GetPostsByFollowedUsersAsync(string currentUserId, int pageNumber, int pageSize)
+        {
+            var followedUserIds = await _context.Follows
+                .Where(f => f.FollowerId == currentUserId)
+                .Select(f=>f.FollowedId)
+                .ToListAsync();
+
+            return await _context.Posts
+                .Where(p => followedUserIds.Contains(p.UserId))
+                .OrderByDescending(p=>p.CreatedAt)
+                .Skip((pageNumber-1)*pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task UpdatePostAsync(Post post)
+        {
+            _context.Posts.Update(post);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ backend/SupportWay.Data/Repositories/Interfaces/IPostsRepository.cs
@@ -0,0 +1,14 @@
+﻿using SupportWay.Data.Models;
+
+namespace SupportWay.Data.Repositories.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetPostsByFollowedUsersAsync(string currentUserId, int pageNumber, int pageSize);
+        Task<IEnumerable<Post>> GetPostByUserAsync(string userId, int pageNumber, int pageSize);
+        Task<Post?> GetPostByIdAsync(string postId);
+        Task AddPostAsync (Post post);
+        Task UpdatePostAsync (Post post);
+        Task DeletePostAsync (int postId);
+    }
+}
